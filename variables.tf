d35dc6d328bf7d2c592181959a1bbfcff3fa1769
efouarge <eric.fouarge@gmail.com>
2019-01-08 13:07:43 -0600

    updating the descriptions on the variables

--- variables.tf
+++ variables.tf
@@ -12,31 +12,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-variable "project" {}
-variable "machine_type" {}
+variable "project" {
+  description = "GCP Project that Spinnaker will be deployed within"
+}
+variable "machine_type" {
+  description = "GCE Instance size that the spinnaker instance(s) will be provisioned with."
+}
 
-variable "dns_zone" {}
+variable "dns_zone" {
+  description = "DNS Zone that will be used to create the Spinnaker service entries"
+}
 
-variable "dns_suffix" {}
+variable "dns_suffix" {
+  description = "The DNS suffix to create the actual records"
+}
 
-variable "region" {}
-variable "zone" {}
+variable "region" {
+  description = "GCP Region that resources will be deployed into"
+}
+variable "zone" {
+  description = "GCP Availability Zones that the instnaces will be deployed within"
+}
 
 variable "network" {
+  description = ""
   default = "default"
 }
 
 variable "protected_networks" {
+  description = "The network CIDRs that will have access to Spinnaker"
   type = "list"
 }
 
-variable "jenkins_master_name" {}
-variable "jenkins_username" {}
-variable "jenkins_password" {}
-variable "jenkins_port" {}
+variable "jenkins_master_name" {
+  description = "Spinnaker resource name for the Jenkins Server you want to register"
+}
+variable "jenkins_username" {
+  description = "Jenkins username to connect to the instance that you defined above"
+}
+variable "jenkins_password" {
+  description = "Jenkins password for the username defined in the jenkins_username"
+}
+variable "jenkins_port" {
+  description = "TCP port that Jenkins is running on"
+}
 
 # Required for Ansible configuration
-variable "ansible_basedir" {}
+variable "ansible_basedir" {
+  description = "This is the location of the ansible playbook on the local disk for the null resource to copy to the GCS bucket"
+}
 
 # rip this out
 variable "basename" {}
